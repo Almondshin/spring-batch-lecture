1ecb8c34042c071f188edc9956df4c9d7b0b077e
almondshin <Almondshin8@gmail.com>
2024-07-11 10:31:45 +0900

    finish

--- src/main/java/io/springbatch/springbatchlecture/TaskletStepConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/TaskletStepConfiguration.java
@@ -3,12 +3,15 @@ package io.springbatch.springbatchlecture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.*;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -16,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -29,8 +33,7 @@ public class TaskletStepConfiguration {
     @Bean
     public Job batchJob() {
         return this.jobBuilderFactory.get("batchJob")
-                .start(taskStep())
-                .next(chunkStep())
+                .start(chunkStep2())
                 .build();
     }
 
@@ -43,17 +46,51 @@ public class TaskletStepConfiguration {
                 })
                 .build();
     }
+
+
+    @Bean
+    public Step taskletStep(){
+        return stepBuilderFactory.get("taskStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("taskStep2 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+
+    @Bean
+    public Step chunkStep2(){
+        return stepBuilderFactory.get("chunkStep2")
+                .<String, String>chunk(10)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String s) throws Exception {
+                        return s.toUpperCase();
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+
+                    @Override
+                    public void write(List<? extends String> list) throws Exception {
+                        list.forEach(item -> System.out.println(item));
+                    }
+                })
+                .build();
+    }
+
+
+
     @Bean
     public Step chunkStep() {
         return stepBuilderFactory.get("chunkStep")
                 .<String, String>chunk(3)
                 .reader(new ListItemReader(Arrays.asList("item1","item2","item3")))
-                .processor(new ItemProcessor<String, String>() {
-                    @Override
-                    public String process(String item) throws Exception {
-                        return item.toUpperCase();
-                    }
-                })
+                .processor((ItemProcessor<String, String>) item -> item.toUpperCase())
                 .writer(list -> {
                     list.forEach(item -> System.out.println(item));
                 })
